3fc33a5ed9882ac0ad4d0b5386ebf19346980a47
srmizaelmoraes <mysael90@gmail.com>
2024-09-03 15:54:47 -0300

    Teste1

---
+++ 1035.py
@@ -0,0 +1,11 @@
+valores = input().split()
+a = int(valores[0])
+b = int(valores[1])
+c = int(valores[2])
+d = int(valores[3])
+sunCD = c + d
+sunAB = a + b
+if (b > c and d > a and sunCD > sunAB and c > 0 and d > 0 and a % 2 == 0):
+    print("Valores aceitos")
+else:
+    print("Valores nao aceitos")
