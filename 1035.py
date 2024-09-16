781c78e5f17185140134530da07e67b0a63a33ef
Mizael A. Silva Moraes <mysael90@gmail.com>
2024-09-16 19:20:56 -0300

    t

--- 1035.py
+++ 1035.py
@@ -8,4 +8,4 @@ sunAB = a + b
 if (b > c and d > a and sunCD > sunAB and c > 0 and d > 0 and a % 2 == 0):
     print("Valores aceitos")
 else:
-    print("Valores nao aceitos")
+    print("Valores nao aceitos") 
