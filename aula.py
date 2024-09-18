8d9ac08e7478ff0b91ef0a18aed7086b96ef1fa3
Mizael A. Silva Moraes <mysael90@gmail.com>
2024-09-17 21:19:42 -0300

    Delete aula.py

--- aula.py
+++
@@ -1,9 +0,0 @@
-c = float(input("Valor da conta: R$ "))
-p = int(input("Quantida de pessoas: "))
-
-calc = c / p
-
-if calc > 50:
-    print("Conta alta")
-else:
-    print("Conta dentro deo esperado!")
