36fd0fcd6ecbc39ac8133ea7b6c51cebf7302336
Mizael A. Silva Moraes <mysael90@gmail.com>
2024-09-19 20:28:40 -0300

    add

--- 1041.py
+++ 1041.py
@@ -3,10 +3,10 @@ x, y = map(float, input().split())
 if x == 0 and y == 0:
     print("Origem")
 elif x == 0:
-    print("Eixo X")
-elif y == 0:
     print("Eixo Y")
-elif y > 0 and y > 0:
+elif y == 0:
+    print("Eixo X")
+elif y > 0 and x > 0:
     print("Q1")
 elif y > 0 and x < 0:
     print("Q2")
