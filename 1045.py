e4863ef1d73abfd2ed3d1039a20492bb536a661f
Mizael A. Silva Moraes <mysael90@gmail.com>
2024-09-24 10:36:18 -0300

    add

--- 1045.py
+++ 1045.py
@@ -1,19 +1,21 @@
-a, b, c = map(float, input().split())
+numbers = list(map(float, input().split()))
 
-if a >= (b + c):
+numbers.sort(reverse = True)
+
+a = numbers[0]
+b = numbers[1]
+c = numbers[2]
+
+if (a >= (b + c)):
     print("NAO FORMA TRIANGULO")
-    
-if (a**2) == ((b**2) + (c**2)):
-    print("TRIANGULO RETANGULO")
-
-if (a**2) > ((b**2) + (c**2)):
-    print("TRIANGULO OBTUSANGULO")
-
-if (a**2) < ((b**2) + (c**2)):
-    print("TRIANGULO ACUTANGULO")
-
+else:
+    if (a**2) == ((b**2) + (c**2)):
+        print("TRIANGULO RETANGULO")  
+    elif (a**2) > ((b**2) + (c**2)):
+        print("TRIANGULO OBTUSANGULO")   
+    else:
+        print("TRIANGULO ACUTANGULO") 
 if a == b and b == c:
-    print("TRIANGULO EQUILATERO")
-
-if (a == b and b != c) or (b == c and c != a) or (c == a and a != b):
+    print("TRIANGULO EQUILATERO")    
+elif (a == b and b != c) or (b == c and c != a) or (c == a and c != b):
     print("TRIANGULO ISOSCELES")
