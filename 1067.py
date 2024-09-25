e981d58a2e19c12039b4a9fc08fb4c939562c87c
Mizael A. Silva Moraes <mysael90@gmail.com>
2024-09-25 13:50:56 -0300

    add

--- 1067.py
+++ 1067.py
@@ -1,6 +1,8 @@
-i = 0
-n = int(input())
-while i <= n:
-    i += 1
-    if (i % 2) == 1:
-        print(i)
+m = -1
+p = -1
+for i in range(1, 101):
+    n = int(input())
+    if n > m:
+        m = n
+        p = i       
+print("{}\n{}".format(m, p))
---
+++ 1080.py
@@ -0,0 +1,6 @@
+i = 0
+maior = 0
+while i <= 100:
+    n = int(input())
+    if n > maior:
+        n = maior
