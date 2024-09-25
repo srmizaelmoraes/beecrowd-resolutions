d3bf5ec712f59431271875ad6355a0970166ca15
Mizael A. Silva Moraes <mysael90@gmail.com>
2024-09-25 16:17:45 -0300

    updade

---
+++ 1094____.py
@@ -0,0 +1,15 @@
+animais = {
+    "C": "Coelho",
+    "R": "Rato",
+    "S": "Sapo"
+}
+i = -1
+totalq = 0
+testes = int(input())
+while i <= testes:
+    q, animal = input().split()
+    if animal == "C":
+        qC = 
+    q = int(q)
+    totalq = totalq + q
+    
