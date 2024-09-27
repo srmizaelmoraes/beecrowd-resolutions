93c3428972a4e1c03c78499b0e0a9db72d38703e
Mizael A. Silva Moraes <mysael90@gmail.com>
2024-09-27 09:56:11 -0300

    update

---
+++ 1114.py
@@ -0,0 +1,5 @@
+tentativa = int(input())
+while tentativa != 2022:
+    print("Senha Invalida")
+    tentativa = int(input())
+print("Acesso Permitido")
--- teste.py
+++
@@ -1,9 +0,0 @@
-m = 0
-i = 0
-for i in range(7):
-    n = float(input()) 
-    if n > 0:
-        i = i + 1
-        m = m + n  
-        media = m / i
-print("{} valores positivos\n{:.1f}".format(i, media))
